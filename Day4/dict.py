d8cb73f8ab98f878fc3ba1b779027278965a1a20
Gautam <gautamnaik2000@gmail.com>
2023-05-30 15:25:19 +0530

    updated Day4/dict.py

---
+++ Day4/dict.py
@@ -0,0 +1,34 @@
+# d = { "name":"Jackson", "follower":666666, "online":True, 34:"id"}
+# for i in d.keys():
+#     print(d[i])
+
+# print(d)
+# print(d.items())
+
+# print(d.keys()) 
+
+# print(d.values())
+
+# print(d["online"])
+# print(d[34])
+
+# d = { "name":"Jackson", "follower":666666, "online":True, 34:{"age":45}}
+# # print(d[34]["age"])
+
+# d[True]='Football'
+# print(d["name"][1:3])
+
+# d = { "name":["Jackson", "Messi"], "follower":666666, "online":True, 34:{"age":45}}
+# print(d[34]["age"])
+
+# d[True]='Football'
+# print(d["name"][1]+"hi")
+
+# d.update({"hobby":"painting"})
+# print(d)
+
+# del d["name"][0]
+# print(d)
+
+# d.clear()
+# print(d)
---
+++ Day4/string.py
@@ -0,0 +1,42 @@
+# hi = " Hello"
+# duniya = "World"
+# # # print(hi[2], hi[-4])
+
+# # # name = input("Enter your Name: ")
+# # # print(hi + name)
+
+# # # hi *= 3
+# # # print(hi)
+
+# # duniya += "r"
+# # print(duniya)
+# duniya = "r"+duniya
+# duniya = 99.9
+# print(duniya)
+
+# ls1 = [1,2,3,4,5]
+# ls2 = [6,7,8,9]
+
+# print(ls1+ls2)
+# - minus not possible in string but multiply is possible
+
+# ls =[1,2,3]
+# print(ls[4])
+
+# string = "Python Lovers"
+
+# for i in range (len(string)):
+#     if string[i] == 'o':
+#         print(i)
+
+# Char = 'o'
+# print(string.find(Char))
+
+# char = 'o'
+# print(string.rfind(char))
+
+# s = "the Avengers"
+# print(s.upper())
+# print(s.lower())
+# print(s.capitalize())
+
