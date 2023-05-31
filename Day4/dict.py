50e88e6c1356ce0b1090522e0ce938af3086256a
Gautam <gautamnaik2000@gmail.com>
2023-05-31 15:14:55 +0530

    Added Day4 Codes

--- Day4/dict.py
+++ Day4/dict.py
@@ -32,3 +32,18 @@
 
 # d.clear()
 # print(d)
+
+# def ping(n):
+#     if(n%2==0):
+#         return True
+#     return False
+
+# def printing(num):
+#     val = ping(num)
+#     if (val==True):
+#         print("Number is Even")
+#     else:
+#         print("Number is odd")
+
+# number = int(input())
+# printing(number)
